47f02e59b902b1800a2559f90a0b2cab4d830a56
Yurii <yuriibel888@gmail.com>
2023-09-19 22:32:02 +0300

    commit again

---
+++ .vite/deps/react-redux.js
@@ -0,0 +1,31 @@
+import {
+  Provider_default,
+  ReactReduxContext,
+  connect_default,
+  createDispatchHook,
+  createSelectorHook,
+  createStoreHook,
+  import_react_dom,
+  shallowEqual,
+  useDispatch,
+  useSelector,
+  useStore
+} from "./chunk-M5VZZRNG.js";
+import "./chunk-OTY6O57J.js";
+import "./chunk-2PA4WPI3.js";
+import "./chunk-ROME4SDB.js";
+var export_batch = import_react_dom.unstable_batchedUpdates;
+export {
+  Provider_default as Provider,
+  ReactReduxContext,
+  export_batch as batch,
+  connect_default as connect,
+  createDispatchHook,
+  createSelectorHook,
+  createStoreHook,
+  shallowEqual,
+  useDispatch,
+  useSelector,
+  useStore
+};
+//# sourceMappingURL=react-redux.js.map
