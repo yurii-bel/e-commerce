80bf1e086ba530b2211555df30b3ccb56a9fa78e
Yurii <yuriibel888@gmail.com>
2023-09-20 19:46:43 +0300

    temp

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,9 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
   },
 };
